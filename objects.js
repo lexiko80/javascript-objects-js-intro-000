d05220250662f6f94cb36f354e911865184f646f
Alexis Kotzambasis <lexiko80@gmail.com>
2019-11-25 03:59:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { '2pac' : 'All Eyez On Me'};
 
 function updatePlaylist(playlist, artist, song){
-  return playlist.assign({}, {artist : song})
+  return Object.assign(playlist, {artist : song})
 }
