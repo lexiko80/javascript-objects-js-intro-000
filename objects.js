3725bd5096736418370a1e09f7a44905cd8aa1fa
Alexis Kotzambasis <lexiko80@gmail.com>
2019-11-25 04:26:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { 'Tupac' : "All Eyez On Me"};
 
 function updatePlaylist(playlist, artist, song){
-  return Object.assign({}, playlist, { artist : song })
+  return Object.assign({}, playlist, { [artist] : song })
 }
